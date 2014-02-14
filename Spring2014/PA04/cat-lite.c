68cbddce3b2bf57a984409ca5cabed5e0133a008
Zach <stephe55@purdue.edu>
2014-02-13 19:34:43 -0500

    grep echo and cat seem to be fully funcitnoal

--- Spring2014/PA04/cat-lite.c
+++ Spring2014/PA04/cat-lite.c
@@ -7,33 +7,54 @@ int main(int argc, char * * argv)
   int ind;
   int num;
   FILE *f;
+  //printf("beginning\n\n\n\n");
+  if(argc==1)
+    {
+      printf("sjdhfkl\n\n\n\n");
+      for(ind = 0;ind<(argc-1);ind++)
+	{
+	  fprintf(stdout,"%s \n",argv[ind]);
+	}
+      return EXIT_SUCCESS;
+    }
   
   for(ind=1;ind<argc;ind++)
     {
-      if( strcmp(argv[ind],"--help")==0)
+      if(strcmp(argv[ind],"-")!=0)
 	{
-	  printf("<help-message>");
-	  return EXIT_SUCCESS;
-	}
-      else
-	{
-	  f = fopen(argv[ind],"r");
-   
-	  if (f==NULL)
+	  if( strcmp(argv[ind],"--help")==0)
 	    {
-	      fprintf(stderr, "cat cannot open %s \n", argv[ind]);
-	      fclose(f);//seg fault here on unopenable files
-	      return EXIT_FAILURE;
+	      printf("<help-message>");
+	      return EXIT_SUCCESS;
 	    }
 	  else
 	    {
-	      while((num=fgetc(f))!= EOF)
+	      f = fopen(argv[ind],"r");
+   
+	      if (f==NULL)
 		{
-		  fputc(num,stdout);
+		  fprintf(stderr, "cat cannot open %s \n", argv[ind]);
+		  return EXIT_FAILURE;
 		}
-	      fclose(f);
+	      else
+		{
+		  while((num=fgetc(f))!= EOF)
+		    {
+		      fputc(num,stdout);
+		    }
+		  fclose(f);
+		}
 	    }
 	}
+      else if (argc==2)
+	{
+	  for(ind=0;ind<argc;ind++)
+	    {
+	      printf("%s ",argv[ind]);
+	    }
+	  printf("\n");
+	}
+	
     }
   return EXIT_SUCCESS;
 }
--- Spring2014/PA04/echo-lite.c
+++ Spring2014/PA04/echo-lite.c
@@ -7,10 +7,10 @@ int main(int argc, char * * argv)
   int ind;
   for(ind = 1;ind<argc; ind++)
     {
-      if(*argv[ind] != '"')
-      {
+      //  if(*argv[ind] != '"')
+	// {
 	printf("%s",argv[ind]);
-      }
+	// }
       printf(" ");
     }
   printf("\n");
